b94b789327fdb436f5d29aa5c28c3bd995070a5e
Anthony <quereantho@gmail.com>
2021-04-02 11:38:21 +0200

    Create the Appointment class

--- src/main/java/com/fges/ckonsoru/models/Appointment.java
+++ src/main/java/com/fges/ckonsoru/models/Appointment.java
@@ -5,4 +5,23 @@ package com.fges.ckonsoru.models;
  * @author anthony
  */
 public class Appointment {
+    /**
+     * Timeslot of the appointment
+     */
+    protected TimeSlot timeslot;
+
+    /**
+     * Client that reserved the appointment
+     */
+    protected Client client;
+
+    /**
+     * Create an appointment from a timeslot and a client
+     * @param timeslot Timeslot of the appointment
+     * @param client Client that reserved the appointment
+     */
+    public Appointment(TimeSlot timeslot, Client client) {
+        this.timeslot = timeslot;
+        this.client = client;
+    }
 }
