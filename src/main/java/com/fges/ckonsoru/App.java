1bf54689037e9b8a653af765eb7beeb8783f0d22
Anthony <quereantho@gmail.com>
2021-05-11 11:42:57 +0200

    Add new UseCases

--- src/main/java/com/fges/ckonsoru/App.java
+++ src/main/java/com/fges/ckonsoru/App.java
@@ -56,10 +56,12 @@ public class App {
 
         // initialating the menu
         UseCase[] actions = {
+                new InitWeek(),
                 new ListFreeTimeslotsByDate(availabilityDAO, appointmentDAO),
                 new ListAppointments(appointmentDAO),
                 new TakeAppointment(availabilityDAO, appointmentDAO),
-                new RemoveAppointment(appointmentDAO)
+                new RemoveAppointment(appointmentDAO),
+                new ListCancellation()
         };
 
         Menu menu = new Menu(actions);
---
+++ src/main/java/com/fges/ckonsoru/usecase/ListCancellation.java
@@ -0,0 +1,13 @@
+package com.fges.ckonsoru.usecase;
+
+public class ListCancellation extends UseCase {
+    @Override
+    public String getChoice() {
+        return "Liste des annulations";
+    }
+
+    @Override
+    public void trigger() {
+        System.err.println("To implement.");
+    }
+}
